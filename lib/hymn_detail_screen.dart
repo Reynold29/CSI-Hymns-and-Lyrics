51d299142971f0b98efb33d6cae79eaa144d9a19
Reynold29 <reynoldclare02@gmail.com>
2024-05-18 14:13:16 +0530

    test: flutter: Implement Audio Playback
    - test: New Test Branch with Beta Features.
    - test: Implement an Audio Playback Feature for Hymns.

--- lib/hymn_detail_screen.dart
+++ lib/hymn_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'hymns_def.dart';
 import 'package:flutter/material.dart';
 import 'package:vibration/vibration.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:favorite_button/favorite_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -19,6 +20,11 @@ class _HymnDetailScreenState extends State<HymnDetailScreen> {
   bool _isFavorite = false;
   double _fontSize = 18.0;
 
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  bool _isPlaying = false;
+
+  final Duration _skipDuration = const Duration(seconds: 5);
+
   void _increaseFontSize() {
     setState(() {
       _fontSize = (_fontSize + 2).clamp(16.0, 40.0);
@@ -35,6 +41,7 @@ class _HymnDetailScreenState extends State<HymnDetailScreen> {
   void initState() {
     super.initState();
     _checkIsFavorite();
+    _initAudioPlayer(); 
   }
 
   Future<void> _checkIsFavorite() async {
@@ -52,12 +59,40 @@ class _HymnDetailScreenState extends State<HymnDetailScreen> {
   }
 
   bool? hasVibrator = await Vibration.hasVibrator();
-  if (hasVibrator != null && hasVibrator) {
-    Vibration.vibrate(duration: 100);
+    if (hasVibrator != null && hasVibrator) {
+      Vibration.vibrate(duration: 100);
+    }
+
+    await _checkIsFavorite();
   }
 
-  await _checkIsFavorite();
-}
+  Future<void> _initAudioPlayer() async {
+    String hymnNumber = widget.hymn.number.toString();
+    String audioUrl = 'https://raw.githubusercontent.com/reynold29/midi-files/main/Hymn_$hymnNumber.ogg';
+
+    try {
+      await _audioPlayer.setAudioSource(AudioSource.uri(Uri.parse(audioUrl)));
+    } catch (e) {
+      print('Error loading audio: $e');
+    }
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  void _toggleAudioPlayback() {
+    setState(() {
+      if (_isPlaying) {
+        _audioPlayer.pause();
+      } else {
+        _audioPlayer.play();
+      }
+      _isPlaying = !_isPlaying;
+    });
+  }
 
   void _showFeedbackDialog() {
     showDialog(
@@ -102,11 +137,11 @@ class _HymnDetailScreenState extends State<HymnDetailScreen> {
   final prefs = await SharedPreferences.getInstance();
   final storedIds = prefs.getStringList('favoriteHymnIds') ?? [];
 
-  if (!storedIds.contains(hymn.number.toString())) { 
-    storedIds.add(hymn.number.toString());
-    await prefs.setStringList('favoriteHymnIds', storedIds);
+    if (!storedIds.contains(hymn.number.toString())) { 
+      storedIds.add(hymn.number.toString());
+      await prefs.setStringList('favoriteHymnIds', storedIds);
+    }
   }
-}
 
   Future<void> _removeFromFavorites(Hymn hymn) async {
     final prefs = await SharedPreferences.getInstance();
@@ -215,6 +250,37 @@ class _HymnDetailScreenState extends State<HymnDetailScreen> {
                   );
                 }),
                 const Spacer(),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.replay_5_rounded),
+                      onPressed: () {
+                        Duration newPosition = _audioPlayer.position - _skipDuration;
+                        if (newPosition < Duration.zero) {
+                          newPosition = Duration.zero; 
+                        }
+                        _audioPlayer.seek(newPosition); 
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+                      onPressed: _toggleAudioPlayback,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.forward_5_rounded),
+                      onPressed: () async {
+                        final currentPosition = _audioPlayer.position;
+                        final newPosition = currentPosition + _skipDuration;
+                        if (newPosition > (await _audioPlayer.duration)!) {
+                          _audioPlayer.stop();
+                        } else {
+                          _audioPlayer.seek(newPosition);
+                        }
+                      },
+                    ),
+                  ],
+                ),
                 Padding(
                   padding: const EdgeInsets.only(top: 10.0),
                   child: SizedBox(
